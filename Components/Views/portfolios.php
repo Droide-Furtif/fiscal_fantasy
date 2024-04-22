eed8d3c09e62f689501f1dd0f7933b1395f62851
Lisa DRIE <lisa.drie.ld@gmail.com>
2024-04-22 10:20:35 +0200

    front des comptes  #13

--- Components/Views/portfolios.php
+++ Components/Views/portfolios.php
@@ -4,19 +4,7 @@
 <html>
 <head>
   <title>Vos comptes</title>
-  <style>
-  .button {
-    display: inline-block;
-    padding: 10px 15px;
-    background-color: blue;
-    color: white;
-    text-decoration: none;
-    border-radius: 5px;
-    cursor: pointer;
-    width: 120px;
-    text-align: center;
-  }
-  </style>
+
 </head>
 <body>
   <br>
@@ -41,7 +29,7 @@
       </select>
       <label name="devise">Devise</label>     
       <br>
-      <input type="submit" required></input>
+      <input class="button" type="submit" required></input>
       </form>
   </div>
 </body>
@@ -52,10 +40,59 @@
         if (elements.style.display === 'none') {
             elements.style.display = 'block';
             document.getElementById('showElementsBtn').innerHTML = "Annuler";
+            document.getElementById('showElementsBtn').classList.remove('ajouter'); // Supprime la classe 'add-account'
+            document.getElementById('showElementsBtn').classList.add('annuler'); // Ajoute la classe 'cancel'
         } else {
             elements.style.display = 'none';
             document.getElementById('showElementsBtn').innerHTML = "Ajouter un compte";
+            document.getElementById('showElementsBtn').classList.remove('annuler'); // Supprime la classe 'cancel'
+            document.getElementById('showElementsBtn').classList.add('ajouter'); // Ajoute la classe 'add-account'
         }
     });
 </script>
-</html>
+</html>  
+<style>
+  #elementsToDisplay{
+    display:flex;
+    justify-content:center;
+  }
+  input{
+    height:40px;
+    width:200px;
+    border: 2px solid  #40852F;
+    border-radius: 10px;
+    margin-bottom: 10px;
+  }
+  select{
+    height:40px;
+    width:200px;
+    border: 2px solid  #40852F;
+    border-radius: 10px;
+    margin-bottom:10px;
+  }
+  .button {
+    display: inline-block;
+    padding: 10px 15px;
+    background-color: #40852F;
+    color: white;
+    text-decoration: none;
+    border-radius: 5px;
+    cursor: pointer;
+    width: 120px;
+    text-align: center;
+    box-shadow: rgba(50, 50, 93, 0.25) 0px 13px 27px -5px, rgba(0, 0, 0, 0.3) 0px 8px 16px -8px;
+    transition: all 0.7s ease;
+  }
+  .button:hover{
+    box-shadow: rgba(50, 50, 93, 0.25) 0px 30px 60px -12px inset, rgba(0, 0, 0, 0.3) 0px 18px 10px -18px inset;
+  }
+  .annuler{
+    width:100px;
+    height:50px;
+    text-align:center;
+    color:white;
+    background-color: #b76b70;
+    border: 5px solid #b76B70;
+    font-size:15px
+  }
+  </style>
